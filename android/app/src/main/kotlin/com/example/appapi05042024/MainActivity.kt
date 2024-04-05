cbcdb7e8d91a231c1a547841c7f95bd310d20c22
aannddrree <aannddrree.silva@gmail.com>
2024-04-05 13:57:01 -0300

    v0

---
+++ android/app/src/main/kotlin/com/example/appapi05042024/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.appapi05042024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Consumo de API',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPosts();
+  }
+
+  Future<void> fetchPosts() async {
+    final response =
+        await http.get(
+          Uri.parse('https://app-uniara-eb91fc9ec7bf.herokuapp.com/list'),
+          headers: {'Content-Type': 'application/json; charset=utf8'}
+          );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        String data = response.body;
+
+        print("antes: $data");
+        data = latin1.decode(latin1.encode(data));
+        print("depois: $data");
+        products = json.decode(data);
+      });
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Produtos'),
+      ),
+      body: ListView.separated(
+        itemCount: products.length,
+        separatorBuilder: (BuildContext context, int index) => Divider(),
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(products[index]['description'])
+          );
+        },
+      ),
+    );
+  }
+}
